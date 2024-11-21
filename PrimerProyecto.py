750d6e44b0daf4c884bb9808944ddfd51727b060
master <aldri@hotmail.es>
2024-11-21 19:19:38 +0100

    ejemplos print

---
+++ PrimerProyecto.py
@@ -0,0 +1,4 @@
+print("Esta es una linea\nY esta es otra")
+# print("Esta es una linea\nY esta es otra") la barra invertida es lo cuenta como caracter
+# especial, y con la n se ejecuta el salto de linea
+print("Me llamo \"Adrian\"")
