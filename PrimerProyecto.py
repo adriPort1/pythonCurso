8db1f0bd54417c9f63dd246c97fd8bc13e5a4503
master <aldri@hotmail.es>
2024-11-21 20:01:19 +0100

    ejercicios y soluciones curso primera hora

--- PrimerProyecto.py
+++ PrimerProyecto.py
@@ -2,3 +2,13 @@ print("Esta es una linea\nY esta es otra")
 # print("Esta es una linea\nY esta es otra") la barra invertida es lo cuenta como caracter
 # especial, y con la n se ejecuta el salto de linea
 print("Me llamo \"Adrian\"")
+print ("\t Esta cuarta para tabular")
+#esta \t es para tabular
+#ejercicio nombres
+#print(input("Dime tu nombre: "))
+#print(input("Dime tu apellido: "))
+print("Tu nombre es "+ input("Dime tu nombre: ")+" "+input("Dime tu apellido: "))
+#ejemplos ejercicios curso
+print(input("¿Qué estás estudiando? "))
+print(input("Escribe tu nombre: ")+" "+input("Escribe tu apellido: "))
+
