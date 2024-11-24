4db6fe734e0352f87bb39884e94c3240c4094fd7
master <aldri@hotmail.es>
2024-11-24 13:58:17 +0100

    mas metodos

--- Dia3/masMetodos.py
+++ Dia3/masMetodos.py
@@ -6,3 +6,23 @@ print(frase)
 print("tristes" in frase)
 print("tristes" not in frase)
 print(len(frase))
+print("pene"*15)
+#listas ----------------------------------------------------
+lista = ["a","b","c"]
+print(type(lista))
+lista2 = ["hola",55,6.1]
+print(lista2)
+print(type(lista2))
+print(len(lista2))
+print(lista2[0:2])
+print(lista+lista2)
+lista2.append("mazorca")
+print(lista2)
+lista2.pop()#asi el ultimo elemento, sino lo que quieras borrar de la lista escibelo
+print(lista2)
+lista3  =  ["a","f","j","b","k"]
+print(lista3)
+lista3.sort()
+print(lista3)
+lista3.reverse()
+print(lista3)
---
+++ Dia3/metodosDiccionario.py
@@ -0,0 +1,21 @@
+#diccionario (dic) ---
+diccionario = {'c1':'valor1','c2':'valor2'} #la clave es c y valor donde pone valor
+print(diccionario)
+resultado = diccionario ['c1']
+print(resultado)
+cliente = {'nombre':'juan','apellido':'perez','peso':88,'talla':1.75}
+consulta = (cliente['apellido'])
+print(consulta)
+dic ={'c1':55,'c2':[10,20,30],'c3':{'s1':100,'s2':200}}
+print(dic['c3']['s2'])
+dic2 = {'c1':[1,2,3],'c2':[4,5,6]}
+print(dic2['c2'][1])#en el ejemplo lo pide en mayus, se pone .upper, el lo hace con strings
+dic3 ={1:'a',2:'b'}
+print(dic3)
+dic3[3]='c'
+print(dic3)
+dic3[2] = 'B'
+print(dic3)
+print(dic3.keys())
+print(dic3.values())
+print(dic3.items())
