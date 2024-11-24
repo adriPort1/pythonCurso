bc8591c8bfe9fed05e457803d1bb7d762bba72fc
master <aldri@hotmail.es>
2024-11-24 12:34:46 +0100

    Mas metodos

---
+++ Dia3/masMetodos.py
@@ -0,0 +1,8 @@
+frase = """tres tristes tigres 
+comen trigo 
+en un trigal"""
+print(frase)
+#o con \n como ya se vio
+print("tristes" in frase)
+print("tristes" not in frase)
+print(len(frase))
--- Dia3/metodos.py
+++ Dia3/metodos.py
@@ -17,3 +17,24 @@ texto= "ABCDEFGHIJKLMNO"
 fragmento2 = texto[2:10:4]
 print(fragmento2)
 #va saltando segun los numeros del ultimo factor, y si pones ::-1 lo pone al reves y segun el numero va saltando tambien
+frase = "Este es el texto de prueba"
+
+pruebaMetodo1= frase[2].upper()#.lower funciona igual pero para minusculas
+pruebaMetodo2 = frase.split()
+pruebaMetodo3 = frase.split("t")
+print(pruebaMetodo1)
+print(pruebaMetodo2)
+print(pruebaMetodo3)
+a = "aprender"
+b = "python"
+c = "es"
+d = "genial"
+e = " ".join([a,b,c,d])
+print(e)
+
+frase2 = "Texto de prueba de la puta hostia"
+
+busqueda = frase2.find("T")
+reemplazar = frase2.replace("puta","maldita")
+print(busqueda)
+print(reemplazar)
