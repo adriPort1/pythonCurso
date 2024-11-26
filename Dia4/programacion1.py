e2e9bb10458bb1c7fa625006293e4ff8ec5e35d4
master <aldri@hotmail.es>
2024-11-26 17:09:00 +0100

    comienzo if

---
+++ Dia4/programacion1.py
@@ -0,0 +1,14 @@
+#esta leccion empezo con comparadores (que ya se, ==, != <=... etc
+# el y es and, o es or y hay not que es como decir qu eno se cumpla que y lo que siga
+#if, elif, else ahora se notara la tabulacion
+if 5==2:
+    print("es correcto")
+else:
+    print("No es correcto")
+mascota = "perro"
+if mascota=="gato":
+    print("Tienes un gato")
+elif mascota == "perro":
+    print("tienes un perro")
+else:
+    print("No se que animal tienes")
