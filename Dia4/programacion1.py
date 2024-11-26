899d9563e8a0eb2e9f6fa3405771871ac132394a
master <aldri@hotmail.es>
2024-11-26 17:39:25 +0100

    receso por furbo

--- Dia4/programacion1.py
+++ Dia4/programacion1.py
@@ -12,3 +12,23 @@ elif mascota == "perro":
     print("tienes un perro")
 else:
     print("No se que animal tienes")
+edad = 16
+nota = 9
+if edad <18:
+    print("Eres menor de edad")
+    if nota >=7:
+        print("aprobado")
+    else:
+        print("No aprobado")
+else:
+    print("eres adulto")
+
+num1 = int(input("Ingresa un número:"))
+num2 = int(input("Ingresa otro número:"))
+
+if num1 > num2:
+        print(f"{num1} es mayor que {num2}")
+elif num2 > num1:
+        print(f"{num2} es mayor que {num1}")
+elif num1 == num2:
+        print(f"{num1} y {num2} son iguales")
